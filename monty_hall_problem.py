5c82706d189a097f29c5da9c9129376bb7644288
Josue G. Gutierrez <josue.gutierrez@mexicoshowroom.com>
2014-02-07 18:15:59 -0600

    Np.Where & mask VS for & random.choice

--- monty_hall_problem.py
+++ monty_hall_problem.py
@@ -9,14 +9,33 @@ def simulate_prizedoor(nsim):
 def simulate_guess(nsim):
 	return np.zeros(nsim,dtype = np.int)
 
-def diff_door(prizes,guesses):
-	r = range(max_doors)
-	response = []	
-	for p,g in it.izip(prizes,guesses):
-		diff = list(set(r)-set([p,g]))
-		choice = np.random.choice(diff,1)[0]
-		response.append(choice)
-	return response
+def goat_door(prizes,guesses):
+	result = np.random.randint(0, 3, prizes.size)
+    	while True:
+        	bad = (result == prizes) | (result == guesses)
+        	if not bad.any():
+            		return result
+        	result[bad] = np.random.randint(0, 3, bad.sum())
+
+#def switch_guess(prizes,guesses):
+#	r = range(max_doors)
+#	response = []	
+#	for p,g in it.izip(prizes,guesses):
+#		diff = list(set(r)-set([p,g]))
+#		choice = np.random.choice(diff,1)[0]
+#		response.append(choice)
+#	return response
+
+def switch_guess(guesses, goatdoors):
+    result = np.zeros(guesses.size)
+    switch = {(0, 1): 2, (0, 2): 1, (1, 0): 2, (1, 2): 1, (2, 0): 1, (2, 1): 0}
+    for i in [0, 1, 2]:
+        for j in [0, 1, 2]:
+            mask = (guesses == i) & (goatdoors == j)
+            if not mask.any():
+                continue
+            result = np.where(mask, np.ones_like(result) * switch[(i, j)], result)
+    return result
 
 def win_percentage(guesses, prizedoors):
 	return 100* (guesses == prizedoors).mean()
@@ -28,7 +47,7 @@ guess =  simulate_guess(nsim)
 
 print "Win percentage when keeping original door ", win_percentage(guess,prizes) 
 
-goats =  diff_door(prizes,guess)
-change = diff_door(guess,goats)
+goats =  goat_door(prizes,guess)
+change = switch_guess(guess,goats)
 
 print "Win percentage when switching doors", win_percentage(change,prizes)
